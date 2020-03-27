ef1caf625672f4a162a0a687c88f063a1fb8a7a9
Giftia <admin@giftia.moe>
2020-03-27 20:42:02 +0800

    暂存，迭代1.7.4

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
-//ChatDACS 1.7.3
+//用户变量和开关，根据你的需要改动
+var version = 'ChatDACS 1.7.4';
+var news_swich = 0;   //是否开启首屏新闻
+var jc_swich = 0; //是否开启酱菜物联服务
+var apikey = '2333333333333333';    //换成你自己申请的 jcck_apikey，非必须
 
+//以下部分非必须则没有必要改动
+
+
+//模块依赖
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var userip = '';
 var net = require('net');
 var request = require('request');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db.db');
-var apikey = '2333333333333333'; //replace your jcck_apikey here
+var db = new sqlite3.Database('db.db'); //数据库位置，默认与index.js同目录
+
+//待改进的变量
+var userip = '';
 var userdata = '';
 var nickname = '';
 var logintimes = '';
 var lastlogintime = '';
-var reg = new RegExp('^/开门 [0-9]*$');
-var rename_reg = new RegExp('^/rename [\u4e00-\u9fa5]*$'); //只允许汉字昵称
-var bv2av__reg = new RegExp('^[a-zA-Z0-9_]{10,12}');  //匹配bv号
 
-http.listen(80, function () {
-    console.log(CurentTime() + '系统启动，正在监听于端口80');
-    Connjc();
-});
+//正则
+var door_reg = new RegExp('^/开门 [a-zA-Z0-9]*$');   //匹配开门
+var rename_reg = new RegExp('^/rename [\u4e00-\u9fa5]*$');  //只允许汉字昵称
+var bv2av__reg = new RegExp('^[a-zA-Z0-9]{10,12}'); //匹配bv号
 
+//代码
 db.run("CREATE TABLE IF NOT EXISTS messages(yyyymmdd char, time char, ip char, message char)");
 db.run("CREATE TABLE IF NOT EXISTS users(nickname char, ip char, logintimes long, lastlogintime char)");
 
+http.listen(80, function () {
+    console.log(Curentyyyymmdd() + CurentTime() + '系统启动，正在监听于端口80');
+    if (jc_swich) {
+        Connjc();
+    }
+});
+
 app.get('/', function (req, res) {
-    var ip = req.headers['x-forwarded-for'] ||
+    var ip = req.headers['HTTP_X_FORWARDED_FOR'] ||
+        req.headers['x-forwarded-for'] ||
         req.ip ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress || '';
+        req.connection.socket.remoteAddress;
     if (ip.split(',').length > 0) {
         ip = ip.split(',')[0];
     };
     ip = ip.replace('::ffff:', '');
     userip = ip;
-    if (userip == ' ') { userip = '未知ip'; };
+    if (userip == ' ' || userip == NaN || userip == undefined || userip == '') { userip = '未知ip'; };
     res.sendFile(__dirname + '/new.html');
 });
 
 io.on('connection', function (socket) {
     GetUserData().then(function (data) {
         io.emit('chat massage', data);
+        if (userip == ' ' || userip == NaN || userip == undefined || userip == '') { userip = '未知ip'; };
         console.log(Curentyyyymmdd() + CurentTime() + '用户 ' + nickname + '(' + userip + ')' + ' 已连接');
         db.run("UPDATE users SET logintimes = logintimes + 1 WHERE ip ='" + userip + "'");
         db.run("UPDATE users SET lastlogintime = '" + Curentyyyymmdd() + CurentTime() + "' WHERE ip ='" + userip + "'");
@@ -54,38 +70,46 @@ io.on('connection', function (socket) {
         lastlogintime = '';
     }, function (err, data) {
         console.log('GetUserData(): rejected, and err:\r\n' + err);
+        if (userip == ' ' || userip == NaN || userip == undefined || userip == '') { userip = '未知ip'; };
         io.emit('system massage', 'GetUserData() err:' + data);
         console.log(Curentyyyymmdd() + CurentTime() + '新用户 ' + userip + ' 已连接');
         db.run("INSERT INTO users VALUES('匿名', '" + userip + "', '1', '" + Curentyyyymmdd() + CurentTime() + "')");
         io.emit('system message', '系统消息：新用户 ' + userip + ' 已连接。你是第一次访问，你可以发送诸如 “/开门 233333” 的通关密码来开门（去掉双引号），密码是基地WiFi密码。');
     });
     io.emit('system message', '系统消息：本项目已开源于<a href="https://github.com/Giftia/ChatDACS/">https://github.com/Giftia/ChatDACS/</a>，欢迎Star');
-    Getnews().then(function (data) {
-        io.emit('chat message', data);
-    }, function (err, data) {
-        console.log('Getnews(): rejected, and err:\r\n' + err);
-        io.emit('system massage', 'Getnews() err:' + data);
-    });
+
+    if (news_swich) {
+        Getnews().then(function (data) {
+            io.emit('chat message', data);
+        }, function (err, data) {
+            console.log('Getnews(): rejected, and err:\r\n' + err);
+            io.emit('system massage', 'Getnews() err:' + data);
+        });
+    };
+
     socket.on('disconnect', function () {
         console.log(Curentyyyymmdd() + CurentTime() + '用户 ' + userip + ' 已断开连接');
         io.emit('system message', '系统消息：用户 ' + userip + ' 已断开连接');
     });
+
     socket.on('typing', function (msg) {
         io.emit('typing', userip + ' 正在输入...');
     });
+
     socket.on('typing_over', function (msg) {
         io.emit('typing', '');
     });
+
     socket.on('chat message', function (msg) {
         msg = msg.replace(/'/g, "[非法字符]");
         //eval(msg); //调试选项，非需要请勿开启
         console.log(Curentyyyymmdd() + CurentTime() + '收到用户 ' + userip + ' 消息: ' + msg);
         db.run("INSERT INTO messages VALUES('" + Curentyyyymmdd() + "', '" + CurentTime() + "', '" + userip + "', '" + msg + "')");
         io.emit('chat message', nickname + '(' + userip + ')' + ' : ' + msg);
-        if (reg.test(msg)) {
+        if (door_reg.test(msg)) {
             if (msg == '/开门 233333') {
                 Opendoor();
-                io.emit('chat message', '系统消息：开门指令已发送');
+                io.emit('chat message', '系统消息：密码已确认，开门指令已发送');
                 io.emit('chat message', '计算机科创基地提醒您：道路千万条，安全第一条。开门不关门，亲人两行泪。');
                 console.log(Curentyyyymmdd() + CurentTime() + '用户 ' + userip + ' 开门操作');
             } else {
@@ -165,31 +189,31 @@ io.on('connection', function (socket) {
     });
 });
 
-function Connjc() {
+function Connjc() { //酱菜物联服务
     var client = new net.Socket();
     client.setEncoding('utf8');
     client.connect(8266, '112.74.59.29', function () {
         client.write('mode=bind&apikey=' + apikey + '&data={ck001000bind}');
-        console.log('绑定成功');
+        console.log(CurentTime() + '酱菜物联服务绑定成功');
     });
     client.on('data', function (data) {
         //console.log(data);
     });
     client.on('error', function (err) {
-        io.emit('绑定错误，错误为 %s', err.code);
-        console.log('绑定错误，错误为 %s', err.code);
+        io.emit('酱菜物联服务绑定错误，错误为 %s', err.code);
+        console.log(Curentyyyymmdd() + CurentTime() + '酱菜物联服务绑定错误，错误为 %s', err.code);
         client.destroy();
     });
 };
 
-function Opendoor() {
+function Opendoor() {   //开门逻辑
     var client = new net.Socket();
     client.setEncoding('utf8');
     client.connect(8266, '112.74.59.29', function () {
         client.write('mode=exe&apikey=' + apikey + '&data={ck0040001}');
         setTimeout(function () {
             client.write('mode=exe&apikey=' + apikey + '&data={ck0040000}');
-            io.emit('chat message', '系统消息：自动关门指令已发送');
+            io.emit('chat message', '系统消息：自动关门指令已发送，仍需手动带门吸合电磁铁');
             console.log(Curentyyyymmdd() + CurentTime() + '自动关门');
         }, 3000);
     });
@@ -197,13 +221,13 @@ function Opendoor() {
         console.log(data);
     });
     client.on('error', function (err) {
-        io.emit('开门错误，错误为 %s', err.code);
+        io.emit('开门错误，错误为 %s，请反馈系统管理员', err.code);
         console.log('开门错误，错误为 %s', err.code);
         client.destroy();
     });
 };
 
-function Curentyyyymmdd() {
+function Curentyyyymmdd() { //年月日
     var now = new Date();
     var year = now.getFullYear();
     var month = now.getMonth() + 1;
@@ -216,9 +240,9 @@ function Curentyyyymmdd() {
         yyyymmdd += '0';
     yyyymmdd += day;
     return (yyyymmdd);
-}
+};
 
-function CurentTime() {
+function CurentTime() { //时分秒
     var now = new Date();
     var hh = now.getHours();
     var mm = now.getMinutes();
@@ -234,7 +258,7 @@ function CurentTime() {
     return (clock);
 };
 
-function Getnews() {
+function Getnews() {    //新闻
     var p = new Promise(function (resolve, reject) {
         request('http://3g.163.com/touch/jsonp/sy/recommend/0-9.html?callback=n', function (err, response, body) {
             if (!err && response.statusCode == 200) {
@@ -255,7 +279,7 @@ function Getnews() {
     return p;
 };
 
-function GetUserData() {
+function GetUserData() {    //写入用户信息
     var p = new Promise(function (resolve, reject) {
         db.all("SELECT * FROM users WHERE ip = '" + userip + "'", function (e, sql) {
             if (!e && sql[0]) {
@@ -273,7 +297,7 @@ function GetUserData() {
     return p;
 };
 
-function Bv2Av(msg) {
+function Bv2Av(msg) {   //BV转AV
     var p = new Promise(function (resolve, reject) {
         request('https://api.bilibili.com/x/web-interface/view?bvid=' + msg, function (err, response, body) {
             body = JSON.parse(body);
