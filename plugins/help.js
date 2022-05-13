fd4b5c446e3a0607a93791767a5ef683f639ae9e
Giftina <admin@giftia.moe>
2022-05-13 14:39:46 +0800

    feature: 帮助指令插件回显增加版本号；

--- plugins/help.js
+++ plugins/help.js
@@ -1,9 +1,9 @@
 module.exports = {
   插件名: "帮助插件", //插件名，仅在插件加载时展示
   指令: "[/!]help|帮助|菜单|插件列表|说明", //指令触发关键词，可使用正则表达式匹配
-  版本: "1.4", //插件版本，仅在插件加载时展示
+  版本: "1.5", //插件版本，仅在插件加载时展示
   作者: "Giftina", //插件作者，仅在插件加载时展示
-  描述: "会回复系统当前可用插件列表", //插件说明，仅在插件加载时展示
+  描述: "会回复系统当前可用插件列表，描述插件版本和对应的触发正则表达式", //插件说明，仅在插件加载时展示
 
   execute: async function (msg, userId, userName, groupId, groupName, options) {
     const plugins = require.all({
@@ -19,7 +19,7 @@ module.exports = {
 
     let pluginList = ["当前插件列表和正则指令：\r\n"];
     for (const i in plugins) {
-      pluginList.push(`${plugins[i].插件名} : ${plugins[i].指令}\r\n`);
+      pluginList.push(`${plugins[i].插件名}_v${plugins[i].版本}: ${plugins[i].指令}\r\n`);
     }
 
     return { type: "text", content: pluginList.toString()?.replace(/,/g, "") };
