ffe010c6a9d38f70d7510d36d785c8e9b5f99b29
Giftina <admin@giftia.moe>
2022-07-07 11:31:41 +0800

    feat: `疯狂星期四` 全匹配视为仅触发语料回复，不学习

--- plugins/crazyThursday.js
+++ plugins/crazyThursday.js
@@ -1,7 +1,7 @@
 module.exports = {
   插件名: "疯狂星期四插件",
   指令: "疯狂星期四",
-  版本: "1.0",
+  版本: "1.1",
   作者: "Giftina",
   描述: "全自动学习关于 `疯狂星期四` 的语料。只要聊天对话中包含了 `疯狂星期四` ，就会自动学习之，并且回复一段其他的语料。",
   使用示例: "世上77亿人，有253亿只鸡，是人数量三倍。如果鸡与人类开战，你必须要对抗3只鸡，就算它死了，又会有同类补上，就算你一个朋友都没有，你还有三只鸡做敌。今天是肯德基疯狂星期四，V我50，我帮你杀敌",
@@ -10,28 +10,31 @@ module.exports = {
   execute: async function (msg, userId, userName, groupId, groupName, options) {
     const teachMsgChecked = msg.replace(/'/g, ""); //防爆
 
-    for (let i in CHAT_BAN_WORDS) {
-      if (
-        teachMsgChecked.toLowerCase().indexOf(CHAT_BAN_WORDS[i].toLowerCase()) !== -1
-      ) {
-        console.log(
-          `疯狂星期四插件: 检测到不允许的词: ${CHAT_BAN_WORDS[i]}，退出教学`.error,
-        );
-        return { type: "text", content: "你教的疯狂星期四里有主人不允许小夜学习的词qwq" };
+    // 对于 "疯狂星期四" 视为仅触发语料回复，不学习
+    if (teachMsgChecked !== "疯狂星期四") {
+      for (let i in CHAT_BAN_WORDS) {
+        if (
+          teachMsgChecked.toLowerCase().indexOf(CHAT_BAN_WORDS[i].toLowerCase()) !== -1
+        ) {
+          console.log(
+            `疯狂星期四插件: 检测到不允许的词: ${CHAT_BAN_WORDS[i]}，退出教学`.error,
+          );
+          return { type: "text", content: "你教的疯狂星期四里有主人不允许小夜学习的词qwq" };
+        }
       }
-    }
-    if (teachMsgChecked.length > 350) {
-      //图片长度差不多是350左右
-      console.log("疯狂星期四插件: 教的太长了，退出教学".error);
-      return { type: "text", content: "你教的疯狂星期四太长了，小夜要坏掉了qwq，不要呀" };
-    }
-    //到这里都没有出错的话就视为没有问题，可以让小夜学了
-    console.log("疯狂星期四插件: 没有检测到问题，可以学习".log);
+      if (teachMsgChecked.length > 350) {
+        //图片长度差不多是350左右
+        console.log("疯狂星期四插件: 教的太长了，退出教学".error);
+        return { type: "text", content: "你教的疯狂星期四太长了，小夜要坏掉了qwq，不要呀" };
+      }
+      //到这里都没有出错的话就视为没有问题，可以让小夜学了
+      console.log("疯狂星期四插件: 没有检测到问题，可以学习".log);
 
-    await ChatModel.create({ ask: "疯狂星期四", answer: teachMsgChecked })
-      .then(() => {
-        console.log("疯狂星期四插件: 学习成功".log);
-      });
+      await ChatModel.create({ ask: "疯狂星期四", answer: teachMsgChecked })
+        .then(() => {
+          console.log("疯狂星期四插件: 学习成功".log);
+        });
+    }
 
     //随机回复一段疯狂星期四
     return { type: "text", content: await utils.FullContentSearchAnswer("疯狂星期四") };
