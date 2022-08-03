f0b419e6049558995f8a9b2e9526d1b05418abf0
Giftina <admin@giftia.moe>
2022-08-03 19:09:40 +0800

    fix: 移除麻烦的指令前缀

--- config/constants.js
+++ config/constants.js
@@ -1,12 +1,12 @@
 module.exports = Object.freeze({
   // 正则表达式
-  change_reply_probability_reg: new RegExp(/^\/回复率 [0-9]*/), // 匹配修改qqBot小夜回复率
-  change_fudu_probability_reg: new RegExp(/^\/复读率 [0-9]*/), // 匹配修改qqBot小夜复读率
+  change_reply_probability_reg: new RegExp(/^回复率 [0-9]*/), // 匹配修改qqBot小夜回复率
+  change_fudu_probability_reg: new RegExp(/^复读率 [0-9]*/), // 匹配修改qqBot小夜复读率
   isImage_reg: new RegExp(/\[CQ:image,file/), // 匹配qq图片消息
   img_url_reg: new RegExp(/https(.*term=3)/), // 匹配图片地址
   isVideo_reg: new RegExp(/\[CQ:video,file:/), // 匹配qq视频消息
   video_url_reg: new RegExp(/http(.*term:unknow)/), // 匹配视频地址
-  come_yap_reg: new RegExp(/^\/嘴臭(.*)/), // 匹配对话语音
+  come_yap_reg: new RegExp(/^嘴臭(.*)/), // 匹配对话语音
   hand_grenade_reg: new RegExp(/^一个手雷(.*)/), // 匹配一个手雷
   mine_reg: new RegExp(/^埋地雷/), // 匹配埋地雷
   fuck_mine_reg: new RegExp(/^踩地雷/), // 匹配踩地雷
@@ -17,9 +17,9 @@ module.exports = Object.freeze({
   i_have_a_friend_reg: new RegExp(/我有.?个朋友说.*/), // 匹配我有个朋友指令
   open_ju_reg: new RegExp(/张菊/), // 匹配张菊指令
   close_ju_reg: new RegExp(/闭菊/), // 匹配闭菊指令
-  gu_gua_reg: new RegExp(/^\/孤寡.*/), // 匹配孤寡指令
+  gu_gua_reg: new RegExp(/孤寡.*/), // 匹配孤寡指令
   fake_forward_reg: new RegExp(/^强制迫害.*/), // 匹配伪造转发指令
-  approve_group_invite_reg: new RegExp(/^\/批准 (.*)/), // 匹配批准加群指令
+  approve_group_invite_reg: new RegExp(/^批准 (.*)/), // 匹配批准加群指令
   reply_reg: new RegExp(/\[CQ:reply,id=.*复读/), // 匹配回复测试指令
 
   // 全局常量
